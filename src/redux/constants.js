4ed684f62db28c15cf032834aafb7d433142ce77
Minh20812 <blossworld12@gmail.com>
2025-03-11 12:43:59 +0700

    update BaseUrl

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,6 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === "production" ? "" : "http://localhost:5000";
 
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://kanbantask-fxzv.onrender.com";
 
 export const USERS_URL = `${BASE_URL}/api/users`;
